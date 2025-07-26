9904b374d8e24467e2091d536ebe87bb7909166e
Chih-Wei,Hsu <f20020303@gmail.com>
2025-07-26 10:39:09 +0800

    data collection

---
+++ API_test.py
@@ -0,0 +1,18 @@
+import requests
+import json
+
+
+# 利用 requests 對 API 來源發送一個請求
+response = requests.get("https://raw.githubusercontent.com/kiang/pharmacies/master/json/points.json")
+
+
+# 將請求回應的內容存成一個字串格式
+data = response.text
+
+# 將長得像 json 格式的字串解析成字典或列表
+data = json.loads(data)
+
+# 印出解析後的資料
+print(data)
+
+
