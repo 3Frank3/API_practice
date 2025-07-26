8921592625e4977baac220c478e2cc71df36bd3e
Chih-Wei,Hsu <f20020303@gmail.com>
2025-07-26 10:53:21 +0800

    mask_count

--- API_test.py
+++ API_test.py
@@ -14,7 +14,7 @@ data = json.loads(data)
 
 # 印出解析後的資料
 #print(data)
-
+'''
 med_count = {}
 
 # 填入欄位名稱
@@ -25,6 +25,24 @@ for d in data['features']:
     med_count[county] += 1
 
 
-print(med_count)
-# {'台北市': 123, '新北市': 456 ...}
+print(med_count) # {'台北市': 123, '新北市': 456 ...}
+'''
 
+# 建立一個空的字典來存放口罩數量
+mask_count = {}
+
+# 填入欄位名稱
+for d in data['features']:
+    county = d['properties']['county']
+    mask_count[county] = d['properties']['mask_adult']
+# 如果字典中沒有這個縣市，就初始化為 0
+    if county not in mask_count:
+        mask_count[county] = 0
+    # 將口罩數量加到對應的縣市
+    mask_count[county] += d['properties']['mask_adult']
+
+
+# 將結果從大到小排列
+mask_count = dict(sorted(mask_count.items(), key=lambda item: item[1], reverse=True))
+
+print(mask_count)# {'台北市': 12345, '新北市': 45678 ...}
